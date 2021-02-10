b3c666a91224b1d82149fc476246c50a7699e859
elsobhano <sobhan23578@gmail.com>
2021-02-10 14:15:36 +0330

    portal doctro

--- DoctorPortal.py
+++ DoctorPortal.py
@@ -4,6 +4,7 @@ import os
 from PyQt5 import uic, QtCore,Qt
 from PyQt5.QtWidgets import QApplication,QGroupBox,QRadioButton,QVBoxLayout ,QLineEdit,QLabel ,QWidget,QPushButton, QMainWindow, QVBoxLayout ,QStackedWidget,QTableWidgetItem
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QDate
 import random
 import datetime
 import matplotlib
@@ -15,6 +16,8 @@ form1=uic.loadUiType(os.path.join(os.getcwd(),"DocPortal.ui"))[0]
 class DocPort(QMainWindow,form1):
     def __init__(self,input ,parent=None):
         super(DocPort, self).__init__(parent)
+        self.setupUi(self)
+        self.updateButton.clicked.connect(self.update)
         print(input)
         self.firstlabel.setText(input[0])
         self.lastlabel.setText(input[1])
@@ -26,12 +29,37 @@ class DocPort(QMainWindow,form1):
         self.pixmap = QPixmap(self.imagepath)
         self.piclabel.setScaledContents(True)
         self.piclabel.setPixmap(self.pixmap)
-        self.updateButton.clicked.connect(self.update)
         self.resumelabel.setText(input[4])
+        d = QDate.currentDate()
+        self.dateEdit.setDate(d)
+    def update(self):
+        print('Here')
+        self.tableWidget.setRowCount(0)
+        self.tabdate = self.dateEdit.date().toString("yyyy-MM-dd")
+        self.conn = sqlite3.connect("appoinment.db")
+        self.c = self.conn.cursor()
+        self.c.execute("SELECT * FROM appoinments WHERE Doc_Name = '{}' AND Date = '{}' ORDER BY date(Date) DESC,CAST(Time AS INTEGER) DESC".format(self.name,self.tabdate))
+        reserve = self.c.fetchall()
+        self.conn.close()
+        self.tableWidget.setRowCount(len(reserve))
+        i = 0
+        k = 0
+        t = datetime.datetime.now()
+        for m in reserve:
+            self.tableWidget.setItem(i,0, QTableWidgetItem(m[0]))
+            self.tableWidget.setItem(i,1, QTableWidgetItem(m[1]))
+            self.tableWidget.setItem(i,2, QTableWidgetItem(m[3]))
+            date_str = m[0]+' '+m[1]
+            date_reserve = datetime.datetime.strptime(date_str,'%Y-%m-%d %H')
+            if date_reserve<t:
+                self.tableWidget.setItem(i,3, QTableWidgetItem('Finished'))
+                k = k + 1
+            i = i + 1
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
-    w = DocPort(("Majid","Samiei","123","09153109973","Jarah","defaultImage.jpg"))
+    w = DocPort(("Amir","Jahanshahi","123","09153109973","Jarah","defaultImage.jpg"))
     w.show()
     sys.exit(app.exec_())
